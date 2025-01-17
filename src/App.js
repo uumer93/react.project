10ae7087de969d11015680ba259f613b9dc41360
uumer93 <sadikomar93@gmail.com>
2025-01-17 17:27:00 +0100

    convert old html/css project to react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,103 @@
 function App() {
   return (
     <>
-      <h1>Branch 1</h1>
-      <h1>Version 2</h1>
-      <h1 className="title">Editing App.js file</h1>
-      <h2>Adding a new header</h2>
+      <header>
+        <h1> My notes </h1>
+      </header>
+      <section>
+        <h2>Ali Hassan</h2>
+        <p style={{color:"green"}}>I will be a web designer after 3 month</p>
+      </section>
+      <main>
+        <section>
+          <h1 className="main-title">CSS course</h1>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="lesson1.html">
+              For more details
+            </a>
+          </article>
+        </section>
+        <section>
+          <h1 style={{ marginTop: 100 }} className="main-title">
+            JavaScript course
+          </h1>
+          <article className="card">
+            <h2 className="lesson-title">Lesson 1</h2>
+            <p className="note">put your note :</p>
+            <a className="details" href="#_blank">
+              For more details
+            </a>
+          </article>
+        </section>
+      </main>
+      <footer>
+        <p>Designed and developed by Ali Hassan © 2020</p>
+      </footer>
     </>
   );
 }
